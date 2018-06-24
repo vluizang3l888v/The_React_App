10779c6f4f8217e61c39d8b8d9db194ddb804bdf
Luis <salazarluis0119@gmail.com>
2018-06-23 20:18:31 -0500

    應對

--- src/App.js
+++ src/App.js
@@ -9,17 +9,40 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+
+state = {
+  persons: [
+    {name: 'Max', age:23 },
+    {name: 'Nando Vlogs', age: 90 },
+    {name: 'Kuan Yang', age:30 }
+
+  ]
+}
+
+
+//become method.
+switchNameHandler = () => {
+  console.log("se hizo clic aquí");
+} 
+
+//<button onClick={this.switchNameHandler()}>Switch Name</button>
+//line 28 is example of line 39. do not use () this will execute im·me·di·ate·ly once react renders this to the DOM. We only want to pass a reference. 
+
   render() {
     return (
       //class cannot be used.
      <div className="App">
       <h1>Hello dude</h1> 
       <p>this is working</p>
-      <Person name="Max" age="23" />
 
-      <Person name="Nando Vlogs" age="90" > My Hobbies: Youtube </Person>
+    
+<button onClick={this.switchNameHandler}>Switch Name</button>
 
-      <Person name="Kuan Yang" age="30" />
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies: Youtube </Person>
+
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
      </div>
 
 //Components are the core building block of React apps
